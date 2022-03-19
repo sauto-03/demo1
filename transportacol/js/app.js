ee137e18c6ba91382209e18423a5e36cd0a99155
sauto-03 <101938977+sauto-03@users.noreply.github.com>
2022-03-19 17:53:43 -0500

    Add files via upload

---
+++ transportacol/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),            
+            new Route('contactos', 'about.html'),
+            new Route('news','news.html')
+        ]);
+    }
+    init();
+}());
+
+/*
+<li></li>
+*/
